cb59241f8f202c2efa68b3b228491085b7bcc36d
Dima Meleshenya <dimasgithub@gmail.com>
2021-07-31 00:49:55 +0300

    Load

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Working_with_flash_drives
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Search_for_disks = new System.Windows.Forms.Button();
+            this.Selecting_a_disk = new System.Windows.Forms.ComboBox();
+            this.Activate_Text = new System.Windows.Forms.Button();
+            this.Entering_the_type = new System.Windows.Forms.TextBox();
+            this.Type_of_Files = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Search_Directory = new System.Windows.Forms.Button();
+            this.Start_Download = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Search_for_disks
+            // 
+            this.Search_for_disks.Location = new System.Drawing.Point(12, 35);
+            this.Search_for_disks.Name = "Search_for_disks";
+            this.Search_for_disks.Size = new System.Drawing.Size(178, 28);
+            this.Search_for_disks.TabIndex = 0;
+            this.Search_for_disks.Text = "Поиск внешних дисков";
+            this.Search_for_disks.UseVisualStyleBackColor = true;
+            this.Search_for_disks.Click += new System.EventHandler(this.Search_for_disks_Click);
+            // 
+            // Selecting_a_disk
+            // 
+            this.Selecting_a_disk.FormattingEnabled = true;
+            this.Selecting_a_disk.Location = new System.Drawing.Point(12, 80);
+            this.Selecting_a_disk.Name = "Selecting_a_disk";
+            this.Selecting_a_disk.Size = new System.Drawing.Size(178, 28);
+            this.Selecting_a_disk.TabIndex = 1;
+            // 
+            // Activate_Text
+            // 
+            this.Activate_Text.Location = new System.Drawing.Point(196, 81);
+            this.Activate_Text.Name = "Activate_Text";
+            this.Activate_Text.Size = new System.Drawing.Size(70, 28);
+            this.Activate_Text.TabIndex = 3;
+            this.Activate_Text.Text = "Выбор";
+            this.Activate_Text.UseVisualStyleBackColor = true;
+            this.Activate_Text.Click += new System.EventHandler(this.Activate_Text_Click);
+            // 
+            // Entering_the_type
+            // 
+            this.Entering_the_type.Location = new System.Drawing.Point(293, 81);
+            this.Entering_the_type.Name = "Entering_the_type";
+            this.Entering_the_type.Size = new System.Drawing.Size(149, 27);
+            this.Entering_the_type.TabIndex = 4;
+            // 
+            // Type_of_Files
+            // 
+            this.Type_of_Files.Location = new System.Drawing.Point(448, 81);
+            this.Type_of_Files.Name = "Type_of_Files";
+            this.Type_of_Files.Size = new System.Drawing.Size(102, 27);
+            this.Type_of_Files.TabIndex = 5;
+            this.Type_of_Files.Text = "Выбор типа";
+            this.Type_of_Files.UseVisualStyleBackColor = true;
+            this.Type_of_Files.Click += new System.EventHandler(this.Type_of_Files_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Выбор типа для переноса файла";
+            // 
+            // Search_Directory
+            // 
+            this.Search_Directory.Location = new System.Drawing.Point(293, 135);
+            this.Search_Directory.Name = "Search_Directory";
+            this.Search_Directory.Size = new System.Drawing.Size(257, 29);
+            this.Search_Directory.TabIndex = 7;
+            this.Search_Directory.Text = "Выбор папки для перемещения ";
+            this.Search_Directory.UseVisualStyleBackColor = true;
+            this.Search_Directory.Click += new System.EventHandler(this.Search_Directory_Click);
+            // 
+            // Start_Download
+            // 
+            this.Start_Download.Location = new System.Drawing.Point(157, 241);
+            this.Start_Download.Name = "Start_Download";
+            this.Start_Download.Size = new System.Drawing.Size(227, 29);
+            this.Start_Download.TabIndex = 0;
+            this.Start_Download.Text = "Начало перевода картинок";
+            this.Start_Download.Click += new System.EventHandler(this.Start_Download_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 327);
+            this.Controls.Add(this.Start_Download);
+            this.Controls.Add(this.Search_Directory);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Type_of_Files);
+            this.Controls.Add(this.Entering_the_type);
+            this.Controls.Add(this.Activate_Text);
+            this.Controls.Add(this.Selecting_a_disk);
+            this.Controls.Add(this.Search_for_disks);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flash drives";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Search_for_disks;
+        private System.Windows.Forms.ComboBox Selecting_a_disk;
+        private System.Windows.Forms.Button Activate_Text;
+        private System.Windows.Forms.TextBox Entering_the_type;
+        private System.Windows.Forms.Button Type_of_Files;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button Search_Directory;
+        private System.Windows.Forms.Button Start_Download;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Working_with_flash_drives
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // Создание переменных;
+        public static string FileD { get; set; }
+        public static string Type_Of_File { get; set; }
+        public static string Name_Disks { get; set; }
+
+
+        // Метод получения названия внешних дисков для дальнейшей работы с данными;
+        private void Search_for_disks_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Очищение при повторном использование;
+                Selecting_a_disk.Items.Clear();
+                // Проверка на количество внешних дисков;
+                foreach (DriveInfo DriverSearch in DriveInfo.GetDrives()) if (DriverSearch.IsReady && DriverSearch.DriveType == DriveType.Removable) Selecting_a_disk.Items.Add(DriverSearch.Name);
+                if (Selecting_a_disk.Items.Count == 0)
+                {
+                    Selecting_a_disk.Text = null;
+                    // Создание ошибки при не обнаружении внешних носителей;
+                    throw new Exception();
+                }
+            }
+            // Вывод ошибки при не обнаружении внешних носителей;
+            catch (Exception)
+            {
+                MessageBox.Show(text: "Проверьте правильность подключения внешних носителей.", caption: "Ошибка при поиске внешних носителей", MessageBoxButtons.OK);
+            }
+        }
+
+
+        private void Activate_Text_Click(object sender, EventArgs e)
+        {
+            // Проверка на количество элементов в грубого говоря списке. Если = 0 - ошибка;
+            try
+            {
+                Name_Disks = Selecting_a_disk.SelectedItem.ToString();
+                MessageBox.Show(text: $"Вы выбрали диск {Name_Disks}.", caption: "Подтверждение действия", MessageBoxButtons.OK);
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show(text: "Вы не ввели внешний носитель.", caption: "Ошибка получения имени внешнего носителя", MessageBoxButtons.OK);
+            }
+        }
+
+
+        // Определение типов файлов;
+        private void Type_of_Files_Click(object sender, EventArgs e)
+        {
+            // Проверка на ввод типов данных;
+            if (Entering_the_type.Text == "")
+            {
+                MessageBox.Show(text: "Вы не ввели тип файла.", caption: "Ошибка получения типа файла", MessageBoxButtons.OK);
+            }
+            // Записываем в переменную текст из Entering_the_type;
+            else Type_Of_File = Entering_the_type.Text;
+            Entering_the_type.Text = null;
+        }
+
+
+        // Открытие и получение основной директории;
+        private void Search_Directory_Click(object sender, EventArgs e)
+        {
+            // Если нажата кнопка выбор папки в в выборе директории, то идет запись;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) FileD = folderBrowserDialog1.SelectedPath + @"\";
+            else
+            {
+                MessageBox.Show(text: "Выберите директорию для перемещение файлов.", caption: "Отмена пользователем", MessageBoxButtons.OK);
+                FileD = null;
+            }
+            // Проверка на правельный выбор директории;
+            if (FileD != null)
+            {
+                if (MessageBox.Show(text: "Вы уверены в правильности выбраной дириктории  для перемещение файлов.", caption: "Подтверждения действия", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    MessageBox.Show(text: "Выбирите нужную директорию для записи.", caption: "Отмена пользователем", MessageBoxButtons.OK);
+                    FileD = null;
+                }
+            }
+        }
+
+
+        // Перенос файлов с обознчением типа;
+        private static void DownloadFilesWithParams()
+        {
+            int count = 0;
+            // Загрузка файлов с определенным типом данных;
+            foreach (var temp in new DirectoryInfo(Name_Disks).GetFiles())
+            {
+                if (Path.GetExtension(temp.Name).Substring(1) == Type_Of_File.ToLower())
+                {
+                    try
+                    {
+                        temp.MoveTo($"{FileD} {temp.Name}");
+                        count++;
+                    }
+                    catch
+                    {
+                        MessageBox.Show(text: $"Файл с названием {temp.Name} уже существует на флешке.", caption: "Ошибка действия", buttons: MessageBoxButtons.OK);
+                    }
+                }
+            }
+            // Проверка на количество файлов перекинутых с флешки;
+            if (count == 0) MessageBox.Show(text: $"На данном внешнем диске нету файлов типа {Type_Of_File} для переноса.", caption: "Ответ системы", buttons: MessageBoxButtons.OK);
+            else MessageBox.Show(text: $"C вашего диска были переписаны файлы типа {Type_Of_File} в количестве {count}.", caption: "Ответ системы", buttons: MessageBoxButtons.OK);
+            Type_Of_File = null;
+        }
+
+
+        // Перенос всех файлов;
+        private static void DownloadFilesWithoutParams()
+        {
+            int count = 0;
+            foreach (var MoveFile in new DirectoryInfo(Name_Disks).GetFiles())
+            {
+                try
+                {
+                    MoveFile.MoveTo($"{FileD} {MoveFile.Name}");
+                    count++;
+                }
+                catch
+                {
+                    MessageBox.Show(text: $"Файл с названием {MoveFile.Name} уже существует на флешке.", caption: "Ошибка действия", buttons: MessageBoxButtons.OK);
+                }
+            }
+            if (count == 0) MessageBox.Show(text: $"На данном внешнем диске нету файлов.", caption: "Ответ системы", buttons: MessageBoxButtons.OK);
+            else MessageBox.Show(text: $"C вашего диска были переписаны все файлы.", caption: "Ответ системы", buttons: MessageBoxButtons.OK);
+        }
+
+
+        // 
+        private void Start_Download_Click(object sender, EventArgs e)
+        {
+            if (Name_Disks == null || FileD == null) MessageBox.Show(text: "Вы не ввели директорию или имя внешнего носителя для переноса файлов", caption: "Ошибка при переносе файлов", MessageBoxButtons.OK);
+            else
+            {
+                // Проверка пользователем правильности выборанного внешнего диска;
+                if (MessageBox.Show(text: $"Вы уверены в правильности выборанного внешних носителей? Вы выбрали внешний носитель {Name_Disks}.", caption: "Подтверждения действия", buttons: MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        // Проверка выбора типа данных пользователем, вызовы методов;
+                        if (Type_Of_File != null)
+                        {
+                            if (MessageBox.Show(text: $"Вы уверены в правильности выбора типа данных, а именно {Type_Of_File}?", caption: "Подтверждения действия", buttons: MessageBoxButtons.YesNo) == DialogResult.Yes)
+                            {
+                                DownloadFilesWithParams();
+                            }
+                            else MessageBox.Show(text: $"Выберите правельный тип данных.", caption: "Отмена пользователем", buttons: MessageBoxButtons.OK);
+                        }
+                        else
+                        {
+                            if (MessageBox.Show(text: "Формат файлов для загрузки не выбран. Хотите загрузить все файлы?", caption: "Подтверждения действия", buttons: MessageBoxButtons.YesNo) == DialogResult.Yes)
+                            {
+                                DownloadFilesWithoutParams();
+                            }
+                            else MessageBox.Show(text: "Выбирите формат для загрузки файлов.", caption: "Отмена пользователем", buttons: MessageBoxButtons.OK);
+                        }
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        MessageBox.Show(text: "Диск был извлечен или сломан во время попытки переноса файлов.", caption: "Ошибка при получения доступа к внешнему носителей", MessageBoxButtons.OK);
+                    }
+                }
+                else MessageBox.Show(text: "Выбирите другой внешний носитель.", caption: "Отмена пользователем", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
+
